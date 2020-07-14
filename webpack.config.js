ae23eda0a25a65a5037eab0f72ee930e2b0f6c5a
Ajay Rajamani <ajayrajamani@gmail.com>
2020-07-13 20:45:04 -0700

    Display mapbox map

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './lib/index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.*']
+    }
+}
